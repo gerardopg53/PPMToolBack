757a9b79af2573e739a21e1f017f51ec62ad07a2
Gerardo Perez <gerris909@Gerardos-MBP.attlocal.net>
2022-01-19 14:24:16 -0700

    initial commit

---
+++ ppmtool/src/test/java/com/gerardopg53/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerardopg53.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
